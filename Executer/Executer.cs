81886440a532ea445d40f6d55a376e42e03c7134
Meisi <meisi.wafiq@hotmail.com>
2019-10-31 10:47:07 +0100

    added reflection to executer

--- Executer/Executer.cs
+++ Executer/Executer.cs
@@ -12,8 +12,20 @@ namespace Executer
             var a = Assembly.LoadFrom("MyLibrary.dll");
             foreach (var type in a.GetTypes())
                 if (type.IsClass)
+                {
+                    var instance = Activator.CreateInstance(type);
+                    foreach(var m in type.GetMethods()) 
+                    {
+                       foreach(var att in m.GetCustomAttributes<ExecuteMe>())
+                        {
+                            m.Invoke(instance, att.Arguments);
+                        }
+                    }
+
+
                     Console.WriteLine(type.FullName);
-            Console.ReadLine();
+                    Console.ReadLine();
+                }
         }
     }
 }
